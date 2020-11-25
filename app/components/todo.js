dcd2c50699254d4fb3e3c93a4f8da4e869a5da7f
Kim Phung <vtkp2002dn@gmail.com>
2020-11-25 20:07:39 +0700

    - add components footer, header, todo, todo-list

---
+++ app/components/todo.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import {inject as service} from '@ember/service';
+
+export default class TodoComponent extends Component {
+  @service('todo-data') todos;
+
+
+}
---
+++ app/services/todo-data.js
@@ -0,0 +1,46 @@
+import Service from '@ember/service';
+import {tracked} from "@glimmer/tracking";
+import {action} from "@ember/object";
+
+export default class TodoDataService extends Service {
+  @tracked todos = [];
+
+  get incomplete() {
+    return this.todos.filter(todo => todo.isCompleted === false);
+  }
+
+  get all(){
+    return this.todos;
+  }
+
+  get todoCountIsLittle() {
+    return this.todos.length <= 1;
+  }
+
+  @action
+  add(text) {
+    let newTodo = new Todo(text);
+    this.todos = [...this.todos, newTodo];
+  }
+
+  @action
+  clearCompleted() {
+    this.todos = this.incomplete;
+  }
+
+  @action
+  toggleCompletion(todo) {
+    todo.isCompleted = !todo.isCompleted;
+  }
+
+}
+
+
+class Todo {
+  @tracked text = '';
+  @tracked isCompleted = false;
+
+  constructor(text) {
+    this.text = text;
+  }
+}
